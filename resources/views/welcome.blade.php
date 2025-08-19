c8f816ca805e543322c4cdd5416d45484c976718
merlindazapata39-design <merlindazapata39@gmail.com>
2025-08-19 15:35:47 -0400

    creando formulario de registro

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
- <h1>INVENTARIO</h1>   
- <h2>Accesorios</h2>
- <H3>Cadenas</H3>
- <h4>Hombre</h4>
- <h4>Mujer</h4>
- <h2>Abarrotes</h2>
- <h3>fideos</h3>
- <div></div>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    </head>
+    <body>
+        <div>
+            <h1>FORMULARIO DE REGISTRO </h1> 
+            <label for=""> Inserte su nombre </label>
+            <input type="text">    
+            <div>
+                <label for=""> Inserte su apellido </label>
+                <input type="text">
+            </div>
+
+            <div>
+                <label for=""> nserte su numero de carnet </label>
+                <input type="number"> 
+            </div>
+
+            <div>
+                <label for="">Fecha de nacimiento</label>
+                <input type="date">  
+            </div>
+
+           <div>
+                <label for="">Inserte su numero de celular</label>
+                <input type="number"> 
+           </div>
+
+            <div>
+                <label for="">Inserte su correo</label>
+                <input type="emaiL">  
+            </div>
+
+            <button>Enviar</button>
+            <button>Borrar formulario</button>
+        </div>
+        <div>
+           <h1>GALERIA DE IMAGENES</h1> 
+           <img src="https://vanacco.com/wp-content/uploads/2020/06/Form-1.jpg" alt="">
+           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQNjxD_8LS5onDYXz32lEpyds1ySaW7vvmsg&s" alt="">
+        </div>
+
+
+        <h2>Accesorios</h2>
+        <H3>Cadenas</H3>
+        <h4>Hombre</h4>
+        <h4>Mujer</h4>
+        <h2>Abarrotes</h2>
+        <h3>fideos</h3>
+    </body>
 </html>
